b3e70438d39fad76f2230b8a1a475d195c1dcf59
IT07 <it07@scarcode.nl>
2016-04-16 23:24:15 +0200

    Version change

--- exile_vemf_reloaded/cpp/CfgPatches.cpp
+++ exile_vemf_reloaded/cpp/CfgPatches.cpp
@@ -6,7 +6,7 @@ class CfgPatches
 		requiredAddons[] = {"exile_server"};
 		fileName = "exile_vemf_reloaded.pbo";
 		requiredVersion = 1.56; // VEMFr does not work on older versions due to use of the latest scripting commands
-		version = 0741.5; // Do NOT change
+		version = 0742.0; // Do NOT change
 		author = "IT07";
 	};
 };
