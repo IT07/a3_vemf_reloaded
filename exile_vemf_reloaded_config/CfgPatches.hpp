5ead4d1f56942bd95bc6cecc364ed96617f85e11
IT07 <it07@scarcode.nl>
2016-06-02 23:21:18 +0200

    Hello

---
+++ exile_vemf_reloaded_config/CfgPatches.hpp
@@ -0,0 +1,10 @@
+class CfgPatches
+{
+	class exile_vemf_reloaded_config
+	{
+		units[] = {};
+		requiredAddons[] = {"exile_server"};
+		fileName = "exile_vemf_reloaded_config.pbo";
+		author = "IT07";
+	};
+};
