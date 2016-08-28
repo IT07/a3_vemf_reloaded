b1416336963181ea9b95b777c6c952eaa0139f9a
IT07 <it07@scarcode.nl>
2016-08-28 16:50:36 +0200

    Version change

--- a3_vemf_reloaded/cpp/CfgPatches.cpp
+++ a3_vemf_reloaded/cpp/CfgPatches.cpp
@@ -6,7 +6,7 @@ class CfgPatches
 		requiredAddons[] = {};
 		fileName = "a3_vemf_reloaded.pbo";
 		requiredVersion = 1.60; // VEMFr does not work on older versions due to use of the latest scripting commands
-		version = "0752.1"; // Do NOT change
+		version = "0752.2"; // Do NOT change
 		author = "IT07";
 	};
 };
